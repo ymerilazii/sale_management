56b5f6ad08061035f1c7a803e370a3b0cef2b2c4
ymerilazii <47615136+ymerilazii@users.noreply.github.com>
2020-03-26 20:07:44 +0100

    Add files via upload

---
+++ sale_management/urls.py
@@ -0,0 +1,31 @@
+"""sale_management URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from nitishop.views import index,calc_page, expense_page,statistics_page,total_page
+
+urlpatterns = [
+    path('', index,name='index'),
+    path('calc/', calc_page,name='calc_page'),
+    path('expense/', expense_page,name='expense_page'),
+    path('statistics/',statistics_page,name='statistics_page'),
+    path('total/',total_page,name='total_page'),
+    path('admin/', admin.site.urls),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
